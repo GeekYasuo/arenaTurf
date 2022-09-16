65760ddfc70c00bc7f323bfa60ce174b860ad9d7
GeekYasuo <96114100+GeekYasuo@users.noreply.github.com>
2022-09-16 11:33:14 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,44 +1,56 @@
-<?php  
+<?php
+require_once('SMTP.php');
+require_once('PHPMailer.php');
+require_once('Exception.php');
 
-//getting user data
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$fromEmail = $_POST['email'];
-$phone = $_POST['tel'];
-$cityName = $_POST['city'];
-$homeAddress = $_POST['address'];
- 
-print_r($fromEmail);
- die;
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\Exception;
 
-//Recipient email, Replace with your email Address
-$mailTo = 'support@arena-turf.games';
- 
-//email subject
-$subject = ' A Booking Request Received From ' .$firstName;
- 
-//email message body
-$htmlContent = '<h2> Email Request Received </h2>
-<p> <b>Client Name: </b> '.$firstName . " " . $lastName . '</p>
-<p> <b>Email: </b> '.$fromEmail .'</p>
-<p> <b>Phone Number: </b> '.$phone .'</p>
-<p> <b>Sports Name: </b> '.$cityName .'</p>
-<p> <b>Time: </b> '.$homeAddress .'</p>';
- 
-//header for sender info
-$headers = "From: " .$firstName . "<". $fromEmail . ">";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
- 
-//PHP mailer function
- $result = mail($mailTo, $subject, $htmlContent, $headers);
- 
-   //error checking
-   if($result) {
-    $success = "The message was sent successfully!";
-   } else {
-    $failed = "Error: Message was not sent, Try again Later";
-   }
+$mail=new PHPMailer(true); // Passing `true` enables exceptions
 
- 
+try {
+    //settings
+    $mail->SMTPDebug=2; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host='smtp.titan.email';
+    $mail->SMTPAuth=true; // Enable SMTP authentication
+    $mail->Username='support@arena-turf.games'; // SMTP username
+    $mail->Password='Sports123456789'; // SMTP password
+    $mail->SMTPSecure='ssl';
+    $mail->Port=465;
+
+    $mail->setFrom('support@arena-turf.games', 'Arena Turf');
+
+    //getting user data
+   $firstName = $_POST['firstName'];
+   $lastName = $_POST['lastName'];
+   $fromEmail = $_POST['email'];
+   $phone = $_POST['tel'];
+   $sportsName = $_POST['city'];
+   $time = $_POST['time'];
+   $date = $_POST['date']
+    
+    //recipient
+    $mail->addAddress($fromEmail, $firstName $lastName);     // Add a recipient
+    
+    //content
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject='Booking Request From '.$firstName.'';
+    $mail->Body='<h2> Email Request Received </h2>
+    <p> <b>Client Name: </b> '.$firstName . " " . $lastName . '</p>
+    <p> <b>Email: </b> '.$fromEmail .'</p>
+    <p> <b>Phone Number: </b> '.$phone .'</p>
+    <p> <b>Sports Name: </b> '.$sportsName .'</p>
+    <p> <b>Date for Booking: </b> '.$date.'</p>
+    <p> <b>Time: </b> '.$time .'</p>';
+
+    $mail->send();
+
+    echo 'Message has been sent';
+} 
+catch(Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: '.$mail->ErrorInfo;
+}
+
 ?>
